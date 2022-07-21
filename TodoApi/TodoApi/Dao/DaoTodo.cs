6757eac6867b78aaa350b6e4c65702db6a31044f
FelipeBraghittoni <felipe.rozzi@auditeste.com.br>
2022-07-21 07:41:48 -0300

    commit1
    bakup

---
+++ TodoApi/TodoApi/Dao/DaoTodo.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Dao
+
+{
+    public class DaoTodo
+    {
+        string conexao = @"Data Source=DESKTOP-HV9333S;Initial Catalog=Todos;Integrated Security=True";
+        public List<TodoItem> GetTodoItems()
+        {
+            List<TodoItem> todoitem = new List<TodoItem>();
+            using (SqlConnection conn = new SqlConnection(conexao))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM TodoItems",conn))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if(reader != null)
+                        {
+                            while (reader.Read())
+                            {
+                                var todoitems = new TodoItem();
+                                todoitems.Name = reader["Nome"].ToString();
+                                todoitems.IsComplete = reader["IsComplete"].is;
+                                todoitem.Add(todoitems);
+                               
+                            }
+                        }
+                    }
+                }
+            }
+                return todoitem;
+        }
+    }
+}
---
+++ TodoApi/TodoApi/Repository/TodoItemRepository.cs
@@ -0,0 +1,22 @@
+﻿using TodoApi.Models;
+using TodoApi.Dao;
+
+namespace TodoApi.Repository
+{
+    public class TodoItemRepository
+    {
+        private readonly DaoTodo _daoTodoItem;
+        public TodoItemRepository()
+        {
+            _daoTodoItem = new DaoTodo();
+        }
+
+        public List<TodoItem> GetTodoItems
+        {
+            get
+            {
+                return _daoTodoItem.GetTodoItems();
+            }
+        }
+    }
+}
